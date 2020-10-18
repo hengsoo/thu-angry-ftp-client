266d655531e72e0551e1f6455e4eb340704b0a27
hengsoo <hengsoo.sjh@gmail.com>
2020-10-18 09:08:36 +0800

    Add login ui

---
+++ angryftp/ftp_client_application.py
@@ -0,0 +1,54 @@
+from tkinter import *
+
+
+class FtpClientApplication:
+    def __init__(self, master):
+        self.addresses = {
+            "ftp_ip": StringVar(value="127.0.0.1"),
+            "ftp_port": StringVar(value=21),
+            "host_ip": "127.0.0.1",
+            "host_port": 21
+        }
+        self.username = StringVar(value="anonymous")
+        self.password = StringVar(value="banana")
+
+        self.master = master
+        self.main_frame = Frame(self.master).pack(padx=2, pady=(0,2))
+        self.ui()
+
+    def ui(self):
+        self.master.title("banana")
+        self.login_ui()
+
+    def login_ui(self):
+        # This will create a LabelFrame
+        login_frame = LabelFrame(self.main_frame, text='Login', padx=5, pady=5)
+        # this wil create a label widget
+        ip_label = Label(login_frame, text="IP:", anchor=W, width=8)
+        ip_input = Entry(login_frame, textvariable=self.addresses["ftp_ip"])
+        port_label = Label(login_frame, text="Port:", anchor=W, width=8)
+        port_input = Entry(login_frame, textvariable=self.addresses["ftp_port"])
+        username_label = Label(login_frame, text="Username:")
+        username_input = Entry(login_frame, textvariable=self.username)
+        password_label = Label(login_frame, text="Password:")
+        password_input = Entry(login_frame, textvariable=self.password, show="*")
+
+        auth_button = Button(self.master, text="Connect", width=20)
+
+        connection_state_label = Label(self.master, text="Disconnected", bg="red", fg="white",
+                                       font='Helvetica 11 bold')
+
+        connection_state_label.pack(side=TOP, fill=BOTH)
+        login_frame.pack(side=TOP, padx=5, pady=2)
+
+        ip_label.grid(row=0, column=0, pady=2)
+        ip_input.grid(row=0, column=1, padx=4)
+        port_label.grid(row=0, column=2)
+        port_input.grid(row=0, column=3)
+
+        username_label.grid(row=1, column=0)
+        username_input.grid(row=1, column=1)
+        password_label.grid(row=1, column=2)
+        password_input.grid(row=1, column=3)
+
+        auth_button.pack(side=TOP, pady=2)
--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import tkinter as tk
+from angryftp.ftp_client_application import FtpClientApplication
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+# Creating tkinter window
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    window = tk.Tk()
+    # window.geometry('550x150')
+    ftp_client = FtpClientApplication(window)
+    window.mainloop()
