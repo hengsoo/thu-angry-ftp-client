aec462bd3c7393324f6d087c2ebd35e67e51bf6b
hengsoo <hengsoo.sjh@gmail.com>
2020-10-19 11:09:44 +0800

    Remove app icon

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from angryftp.ftp_client_application import AngryFtpClientApplication
 # Creating tkinter window
 if __name__ == '__main__':
     window = tk.Tk()
-    window.iconbitmap("D:/Hengsoo/Desktop/ftp-client/angryftp/angry.ico")
     window.resizable(False, False)
     AngryFtpClientApplication(window)
     window.mainloop()
