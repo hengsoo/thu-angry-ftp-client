a12a46aba81e7e0bee63ca1db57258cf83d30179
hengsoo <hengsoo.sjh@gmail.com>
2020-10-19 10:54:48 +0800

    Compile to onefile

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from angryftp.ftp_client_application import AngryFtpClientApplication
 # Creating tkinter window
 if __name__ == '__main__':
     window = tk.Tk()
+    window.iconbitmap("D:/Hengsoo/Desktop/ftp-client/angryftp/angry.ico")
     window.resizable(False, False)
     AngryFtpClientApplication(window)
     window.mainloop()
